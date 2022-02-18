f8acc14abc8bf907dbea261b451142bb8790ce3f
Yeji <urmetjslia@gmail.com>
2022-02-18 15:17:17 +0900

    정렬

--- Bydin/src/main/java/com/Bydin/controller/AjaxController.java
+++ Bydin/src/main/java/com/Bydin/controller/AjaxController.java
@@ -1,20 +1,31 @@
 package com.Bydin.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.Bydin.Ajax.AjaxService;
+import com.Bydin.Service.ItemService;
+import com.Bydin.item.TotalGoodsDTO;
 import com.Bydin.member.MemberDTO;
 
 @RestController
 public class AjaxController {
 	@Autowired private AjaxService as;
+	@Autowired private ItemService is;
 	
 	@GetMapping("checkuserid")
 	public int checkuserid(String userid) {
 		MemberDTO dto = as.selectuserid(userid);
 		return dto != null ? 1 : 0;
+	}
+	
+	@GetMapping("item/dto1")
+	public List<TotalGoodsDTO> dto1(){
+		List<TotalGoodsDTO> list = is.getStudyroomItem();
+		return list;
 	}
 	
 }
